dd36aa6301ae9d05fa1553cc106fc22da27841bb
allanbarbozag <allpatrick.b@gmail.com>
2022-05-20 22:42:04 -0300

    Incrementa a funcionalidade do email-controller

--- src/front/controllers/email-controller.js
+++ src/front/controllers/email-controller.js
@@ -1,13 +1,39 @@
 class EmailController {
+
   constructor() {
     this.bancoEmails = [];
-  }
+  };
 
   validaEmail() {
     let emailInput = document.getElementById('email-input').value;
-    const novoEmail = new EmailModel(emailInput);
-    this.bancoEmails.push(novoEmail);
+    let novoEmail = new EmailModel(emailInput);
+    const novoEmailView = new EmailView();
     
-  }
+    if (novoEmail.email === undefined) {
+      console.log('email invalido')
+      novoEmailView.invalidaEmail()
+    } else {
+      this.bancoEmails.push(novoEmail);
+      novoEmailView.confirmaEmail(novoEmail.email);
+      
+      console.log(novoEmail)
+      console.log(novoEmail.email)
+      console.log(this.bancoEmails)
+    }
+ };
+
+ exibeAlerta() {
+
+ };
 }
+
+const emailController = new EmailController();
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  emailController.validaEmail();
+
+  
+  
+})
 
