8b0d42fb64501e8a482c5c80cdb562e3dd9794fb
allanbarbozag <allpatrick.b@gmail.com>
2022-05-26 17:16:28 -0300

    Corrige o header do redirecionamento de página nas página de login e recsenha.

--- src/front/views/email-view.js
+++ src/front/views/email-view.js
@@ -11,27 +11,27 @@ class EmailView {
 
     <header>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <img id="logoCanarinho" src="/Canarinho-Bot/src/img/CanarinhoBotSized.png" width="225" class="d-inline-block align-text-top .ms-n1 cor-texto">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul class="navbar-nav d-flex justify-content-between" >
+          <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link"  id="home" aria-current="page" href="#">Home</a>
+              <a class="nav-link active cor-texto" id="home" aria-current="page" href="/Canarinho-Bot/index.html">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link cor-texto" id="subscribe" href="#">Inscreva-se</a>
+              <a class="nav-link cor-texto" id="subscribe" href="/Canarinho-Bot/src/html/form.html">Inscreva-se</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link cor-texto" id="profile" href="#">Quem Somos</a>
+              <a class="nav-link cor-texto" id="profile" href="/Canarinho-Bot/src/html/planos.html">Quem Somos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link cor-texto" id="contact" href="#">Contato</a>
+              <a class="nav-link cor-texto" id="contact" href="/Canarinho-Bot/src/html/contato.html">Contato</a>
             </li>
             <li class="nav-item">
-              <button type="button" class="btn btn-outline-warning .col-6 .mx-auto"><a class="cor-texto" href="paginadelogin.html">Login</a></button>
+              <button type="button" class="btn btn-outline-warning .col-6 .mx-auto"><a class="cor-texto" href="/Canarinho-Bot/src/html/login.html">Login</a></button>
             </li>
           </ul>
         </div>
